c80c5cf403d728beca6d0116248571ebb523776c
Devon Call <callbrentdevon@gmail.com>
2018-08-23 22:42:52 -0700

    First Commit, basic skeleton

---
+++ app/static/js/index.js
@@ -0,0 +1,14 @@
+let index = {
+  init: function() {
+    asticode.loader.init();
+    asticode.modaler.init();
+    asticode.notifier.init();
+
+    document.addEventListener('astilectron-ready', function() {
+      // This will send a message to GO
+      astilectron.sendMessage({name: "event.name", payload: "hello"}, function(message) {
+          console.log("received " + message.payload)
+      });
+    })
+  }
+};
